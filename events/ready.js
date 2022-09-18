6f227df70a6bb958a40c4fd3fa9d9cc067ef0d67
tanrisiz <113816295+tanrisiz@users.noreply.github.com>
2022-09-18 15:10:19 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
-const moment = require('moment');
-const Discord = require('discord.js');
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
 const ayarlar = require('../ayarlar.json');
 module.exports = async client => {
-  client.user.setPresence({ activity: { type: "playing", name: `@tnrsz1940 `}, status: 'dnd' })
+  client.user.setPresence({ activity: { type: "LISTENING", name: `Tanrısız ❤️ 1940 `}, status: 'dnd' })
 };
