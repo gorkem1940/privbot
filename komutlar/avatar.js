a19923d3907763bcb6b1434dbd0660d5442047b5
gorkem <113816295+gorkem1940@users.noreply.github.com>
2025-01-28 13:48:01 +0300

    avatar.js

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -19,7 +19,7 @@ exports.run = async (client, message, args) => {
       url: user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 })
     },
     footer: {
-      text: "tanrısız",
+      text: "Görkem",
       icon_url: client.user.avatarURL({ format: 'png' })
     },
     
